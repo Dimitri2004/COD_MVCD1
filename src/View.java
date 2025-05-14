ee2848fc59f829c4f270a3edb593f3c9e78c93a5
Dima <dapariciopereira@danielcastelao.org>
2025-05-14 13:17:55 +0200

    MVC 0.10

--- src/View.java
+++ src/View.java
@@ -65,7 +65,5 @@ public class View {
     /*
     public static boolean muestraVelocidad(String matricula, Integer v){
         System.out.println(matricula + ": " + v + "km/hr");
-        return true;
+        return true;*/
     }
-     */
-}
